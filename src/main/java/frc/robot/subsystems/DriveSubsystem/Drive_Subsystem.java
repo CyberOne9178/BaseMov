98d63cff7fe07f7c3a78dd39056930574aed38fe
Cyber One <cyberonefrc@gmail.com>
2026-01-12 14:24:11 -0300

    CyberONE

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem/Drive_Subsystem.java
@@ -0,0 +1,38 @@
+  // Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.DriveSubsystem;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drive_Subsystem extends SubsystemBase {
+
+DifferentialDrive mover;  
+
+SparkMaxConfig config1 = new SparkMaxConfig();
+SparkMaxConfig config2 = new SparkMaxConfig();
+
+   SparkMax[] arrayMovimento = new SparkMax[4];
+        
+  public Drive_Subsystem() {
+
+  config1.follow(1);
+  config2.follow(2);
+    for(int i = 0; i < arrayMovimento.length; i++ ) {
+        arrayMovimento[i] = new SparkMax(i + 1, MotorType.kBrushed);
+        }
+
+    mover = new DifferentialDrive(arrayMovimento[0], arrayMovimento[2]);
+
+      }
+
+  @Override
+  public void periodic() {
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem/Pigeon.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.DriveSubsystem;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Pigeon extends SubsystemBase {
+  public static Pigeon2 pigeon;
+
+  /** Creates a new Pigeon. */
+  public Pigeon() {
+    pigeon = new Pigeon2(1);
+  }
+
+public double getYaw() {
+  return pigeon.getYaw().getValueAsDouble();
+  }
+    public void setYaw(double angle) {
+    pigeon.setYaw(angle);
+  }
+
+  public double getPitch() {
+    return pigeon.getPitch().getValueAsDouble();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
